5db4396978418616f9ee77fd851c372b96fdb9a3
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2022-08-11 14:44:27 +0900

    :pencil2: fixing typos
    typo fix in main.py

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 """FastAPI is a blazing fast, simple, and flexible API Framework for Python."""
 
-
-from fastapi import FastAPI
+from starlette.applications import Starlette
+from starlette.routing import Route, Mount
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 from enum import Enum
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+    vgg = "vgg"
 
-app=FastAPI()
+templates = Jinja2Templates(directory='static/templates')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
-@app.get("/users/me")
-async def read_user_me():
-    return {"user": "the current user"}
-@app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id}
-@app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-@app.get('/models/{model_name}')
-async def get_model(model_name: ModelName):
-    if model_name==ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name==ModelName.lenet:
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    if model_name==ModelName.resnet:
-        return {"model_name": model_name, "message": "Just a plain Resnet-101"}
-    return {"model_name": model_name, "message": "Unknown model name"}
+async def homepage(request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+routes = [
+    Route('/', endpoint=homepage),
+    Mount('/static', StaticFiles(directory='static'), name='static')
+]
+
+app = Starlette(debug=True, routes=routes)
