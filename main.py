5b7f038b5eef8ce8b88ab704bb67652310d941c8
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2022-08-14 19:20:39 +0900

    fixing typo

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from enum import Enum
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
     vgg = "vgg"
 
+
 templates = Jinja2Templates(directory='static/templates')
+
 
 async def homepage(request):
     return templates.TemplateResponse('index.html', {'request': request})
